aaad8305d78c0e9bb32fe27820f13ecd738b803a
thetinahang <thetinahang@gmail.com>
2017-07-31 21:53:11 -0700

    saving, did not finish

---
+++ lib/materials.rb
@@ -0,0 +1,2 @@
+require_relative './materials/data/version'
+require_relative './materials/data/cli'
--- lib/materials/data.rb
+++ lib/materials/data.rb
@@ -1,7 +1,26 @@
 require "materials/data/version"
+require "nokogiri"
 
 module Materials
   module Data
     # Your code goes here...
+    attr_accessor :keyword
+
+    def initialize(keyword)
+    	@keyword = keyword
+    end
+
+    def self.all
+    end
+
+    def self.find_by_keyword(keyword)
+    	self.all.detect do |item|
+    		item.keyword.downcase.strip == keyword.downcase.strip
+    	end
+    end
+
+
+
+
   end
 end
